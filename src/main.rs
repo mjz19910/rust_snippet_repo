5173f866ec840752b0e7fa8b50a6d1e6135c39ae
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-28 23:09:36 -0700

    u

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,11 @@
 #![feature(once_cell)]
 #![feature(extern_types)]
 #![feature(let_chains)]
+
 pub mod exec_mode;
-/// #![feature(once_cell)] for `use std::cell::LazyCell;`
-/// #![feature(generic_const_exprs)] for `let x:Type<{ N + {number}}>`
-/// #![allow(incomplete_features)] for #![feature(generic_const_exprs)]
-/// #![feature(ptr_metadata)] for `use std::ptr::DynMetadata;`
-/// #![feature(unsize)] for `use std::marker::Unsize;`
-/// #![feature(let_chains)] for `if let .. = .. && ..`
 pub mod modes;
 pub mod support;
+
 use exec_mode::exec_mode;
 
 pub fn main() -> Result<(), String> {
