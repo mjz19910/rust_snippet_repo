8fd063923b9e12cc985686111ddefb91be433745
Matthias Zimmerman <matthias291999@gmail.com>
2022-11-04 08:23:05 -0700

    u

--- examples/jello-vm-rs/main.rs
+++ examples/jello-vm-rs/main.rs
@@ -1039,32 +1039,31 @@ fn execute_instruction(
         };
         let name_of_class = get_name_of_class(clazz, class_index);
         let name_of_member = get_name_of_member(clazz, name_and_type_index);
-        if name_of_class == "java/io/PrintStream" && name_of_member == "println" {
-            let n = stack.len();
-            if stack.len() < 2 {
-                panic!("RuntimeError({name_of_class}/{name_of_member} expects 2 arguments, but provided {n})");
-            }
-            let obj = &stack[stack.len() - 2];
-            let dsc = mem::discriminant(obj);
-            if mem::discriminant(&JavaValue::FakePrintStream) != dsc {
-                panic!("Unsupported stream type {dsc:?}");
-            };
-            let arg = stack[stack.len() - 1].clone();
-            if let JavaValue::String { value } = arg {
-                println!("{}", value);
-            } else if let JavaValue::Integer { value } = arg {
-                println!("{}", value);
-            } else if let JavaValue::Double { value } = arg {
-                println!("{:?}", value);
-            } else if let JavaValue::Float { value } = arg {
-                println!("{:?}", value);
-            } else if let JavaValue::Long { value } = arg {
-                println!("{}", value);
-            } else {
-                panic!("Support for {arg:?} is not implemented");
-            }
-        } else {
+        let false = (name_of_class == "java/io/PrintStream" && name_of_member == "println") else {
             panic!("Unknown method {name_of_class}/{name_of_member} in invokevirtual instruction");
+        };
+        let n = stack.len();
+        if stack.len() < 2 {
+            panic!("RuntimeError({name_of_class}/{name_of_member} expects 2 arguments, but provided {n})");
+        }
+        let obj = &stack[stack.len() - 2];
+        let dsc = mem::discriminant(obj);
+        if mem::discriminant(&JavaValue::FakePrintStream) != dsc {
+            panic!("Unsupported stream type {dsc:?}");
+        };
+        let arg = stack[stack.len() - 1].clone();
+        if let JavaValue::String { value } = arg {
+            println!("{}", value);
+        } else if let JavaValue::Integer { value } = arg {
+            println!("{}", value);
+        } else if let JavaValue::Double { value } = arg {
+            println!("{:?}", value);
+        } else if let JavaValue::Float { value } = arg {
+            println!("{:?}", value);
+        } else if let JavaValue::Long { value } = arg {
+            println!("{}", value);
+        } else {
+            panic!("Support for {arg:?} is not implemented");
         }
     } else if let Opcode::getstatic = opcode {
         let index = parse_u2_vec(index, &code);
