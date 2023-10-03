59c7ab606e4d4d0dcd2bbec36da4ca04b27a750d
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-03 02:39:07 -0700

    u

--- src/modes/lambda_ref.rs
+++ src/modes/lambda_ref.rs
@@ -25,36 +25,41 @@ pub fn lambda_ref() {
     let lambda_x = 0x4151u64;
     let lambda_z = 0u64;
     let lambda = || (lambda_a, lambda_b, lambda_x, lambda_z);
-    let c = ptr::addr_of!(lambda);
+    let fn_ptr = &lambda as &dyn FnOnce() -> (u64, fn(), u64, u64);
+    fn show_val<'a, T>(value: T) -> Vec<&'a [u64]> {
+        let c = ptr::addr_of!(value);
+        let a = c as *const *const u64;
+        let size = mem::size_of_val(&value);
+        let slice_len = size / 8;
+        let lambda_parts = unsafe { slice::from_raw_parts(a, slice_len) };
+        let mut ret_parts = vec![];
+        for item in lambda_parts {
+            let slice = unsafe { slice::from_raw_parts(*item, 1) };
+            ret_parts.push(slice);
+        }
+        ret_parts
+    }
+    let c = ptr::addr_of!(fn_ptr);
     let a = c as *const *const u64;
-    let size = mem::size_of_val(&lambda);
+    let size = mem::size_of_val(&fn_ptr);
     let slice_len = size / 8;
     let lambda_parts = unsafe { slice::from_raw_parts(a, slice_len) };
     println!(
-        "addr of lambda info: {:x?} {:#x} {:x?}",
+        "addr of lambda info: {:x?}[{:#x}]={{{:x?}}}",
         c, size, lambda_parts
     );
-    println!("read all captured refs: {:x?}", unsafe {
-        (
-            slice::from_raw_parts(lambda_parts[0], 1),
-            slice::from_raw_parts(lambda_parts[1], 1),
-            slice::from_raw_parts(lambda_parts[2], 1),
-            slice::from_raw_parts(lambda_parts[3], 1),
-        )
-    });
-    let func = gdb_bp as extern "C" fn();
-    let func_size = mem::size_of_val(&func);
-    let f_ptr = ptr::addr_of!(func);
-    let ptr = f_ptr as *const u64;
-    println!("f_ptr: {:#x?}[{:#x}]", unsafe { *ptr }, func_size);
-    let func = read_as_optional(f_ptr).unwrap();
-    func();
-    let value = unsafe { *(lambda_parts[1] as *const usize) };
-    match value {
-        0 => println!("[null]"),
-        _ if value > 0x1000 && value < 0x7ffffffff000 => (),
-        _ => println!("*lambda_parts[1]: [{:x?}]", value),
-    }
-    let ret = lambda();
-    println!("(ret.1): {:x?}[{:#x?}]", ret.1, mem::size_of_val(&ret.1));
+    println!("read all captured refs: {:x?}", show_val(fn_ptr));
+    let gdb_bp_fn = gdb_bp as extern "C" fn();
+    let func_size = mem::size_of_val(&gdb_bp_fn);
+    let gdb_bp_ptr = ptr::addr_of!(gdb_bp_fn);
+    let gdb_bp_ptr_u64 = gdb_bp_ptr as *const u64;
+    println!(
+        "f_ptr: {:#x?}[{:#x}]",
+        unsafe { *gdb_bp_ptr_u64 },
+        func_size
+    );
+    let gdb_bp_fn = read_as_optional(gdb_bp_ptr).unwrap();
+    gdb_bp_fn();
+    let (_ret_a, ret_b, _ret_x, _ret_z) = lambda();
+    println!("(ret.1): {:x?}[{:#x?}]", ret_b, mem::size_of_val(&ret_b));
 }
