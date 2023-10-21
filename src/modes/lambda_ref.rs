bcd2211d56b72dec5155552eade8080b490c8abd
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-20 21:08:25 -0700

    u

--- src/modes/lambda_ref.rs
+++ src/modes/lambda_ref.rs
@@ -74,8 +74,7 @@ pub fn lambda_ref() {
     println!("fn_ptr_data2.1={fn_ptr_data_vec2:x?}");
     let gdb_bp_fn = gdb_bp as extern "C" fn();
     assert_eq!(size_of_val(&gdb_bp_fn), 8);
-    let gdb_bp_ptr = addr_of!(gdb_bp_fn);
-    let gdb_bp_ptr_u64 = gdb_bp_ptr.cast::<u64>();
+    let gdb_bp_ptr_u64 = addr_of!(gdb_bp_fn).cast::<u64>();
     println!("gdb_bp_fn: {:#x?}", unsafe { *gdb_bp_ptr_u64 });
     read_as_optional(addr_of!(gdb_bp_fn)).unwrap()();
     let (_ret_a, ret_b, _ret_x, _ret_z) = lambda();
