68a764eccaa37f25971f84c39bd30f35a103d34c
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-21 15:18:05 -0700

    u

--- src/support/l/async_vec.rs
+++ src/support/l/async_vec.rs
@@ -3,12 +3,12 @@ use std::pin::Pin;
 use std::task::Context;
 use std::task::Poll;
 
-use crate::support::waker_utils::simple_waker_into_waker;
+use crate::support::waker_utils::noop_waker;
 
 pub fn async_vec() {
     let mut value = async { vec![1, 2, 3] };
     let pin_value = unsafe { Pin::new_unchecked(&mut value) };
-    let waker = simple_waker_into_waker(&());
+    let waker = noop_waker();
     let res = pin_value.poll(&mut Context::from_waker(&waker));
     let Poll::Ready(value) = res else {
         panic!("Future not ready");
--- src/support/l/waker_utils.rs
+++ src/support/l/waker_utils.rs
@@ -1,13 +1,19 @@
+use core::ptr::null;
 use std::task::{RawWaker, RawWakerVTable, Waker};
 
-const VTABLE: RawWakerVTable =
-    unsafe { RawWakerVTable::new(|s| simple_waker_clone(&*s), |_s| (), |_s| (), |_s| ()) };
-
-fn simple_waker_clone(s: &()) -> RawWaker {
-    RawWaker::new(s, &VTABLE)
+unsafe fn noop_clone(_data: *const ()) -> RawWaker {
+    noop_raw_waker()
 }
 
-pub fn simple_waker_into_waker(s: &()) -> Waker {
-    let raw_waker = RawWaker::new(s, &VTABLE);
+unsafe fn noop(_data: *const ()) {}
+
+const NOOP_WAKER_VTABLE: RawWakerVTable = RawWakerVTable::new(noop_clone, noop, noop, noop);
+
+const fn noop_raw_waker() -> RawWaker {
+    RawWaker::new(null(), &NOOP_WAKER_VTABLE)
+}
+
+pub fn noop_waker() -> Waker {
+    let raw_waker = noop_raw_waker();
     unsafe { Waker::from_raw(raw_waker) }
 }
