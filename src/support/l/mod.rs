2bccbf6727e69088ec2d668016b95cad83dc428c
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-21 16:14:32 -0700

    u

--- src/support/l/mod.rs
+++ src/support/l/mod.rs
@@ -1,24 +1,17 @@
-#[macro_export]
 macro_rules! export1 {
     ($body:tt) => {
         mod $body;
 				pub(crate) use $body::$body;
     };
 }
-pub mod box_;
-mod cmd_arg;
-pub use cmd_arg::CmdArg;
+
 export1!(async_vec);
-pub mod constants;
-pub mod describe;
-pub mod drop_helpers;
 export1!(elf_base);
 export1!(find_next_object);
 export1!(get_command_line_arguments);
 export1!(get_debug_flag_state);
 export1!(get_type);
 export1!(handle_current_object);
-pub mod internal_types;
 export1!(iter_find_next_object);
 export1!(iter_type);
 export1!(loop_branch_1);
@@ -26,12 +19,20 @@ export1!(loop_branch_2);
 export1!(loop_branch_4);
 export1!(loop_inner_1);
 export1!(loop_inner_3);
+export1!(p_dbg);
+export1!(print_debug_state);
+
+pub mod box_;
+mod cmd_arg;
+pub use cmd_arg::CmdArg;
+pub mod constants;
+pub mod describe;
+pub mod drop_helpers;
+pub mod internal_types;
 mod loop_state;
 pub use loop_state::LoopState;
 pub mod mark_offset_hit;
 pub mod metadata;
-export1!(p_dbg);
-export1!(print_debug_state);
 mod ptr_iter;
 pub use ptr_iter::PtrIter;
 pub mod ptr_math;
