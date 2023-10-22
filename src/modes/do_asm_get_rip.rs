15da7c07243fe716ddd769e5a3fbe9922fbe21c8
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-21 17:10:59 -0700

    u

--- src/modes/do_asm_get_rip.rs
+++ src/modes/do_asm_get_rip.rs
@@ -1,5 +1,6 @@
 use std::arch::asm;
 
+#[inline(never)]
 pub fn asm_get_rip() -> u64 {
     let mut value: u64;
     unsafe {
@@ -8,10 +9,12 @@ pub fn asm_get_rip() -> u64 {
     value
 }
 
+#[inline(never)]
 pub fn do_asm_get_rip() {
     let ptr = &(asm_get_rip as fn() -> _) as &fn() -> _ as *const fn() -> _;
     let ptr_1 = ptr as *const fn() -> u64;
     println!("fn_ptr  : {:#x}", asm_get_rip as fn() -> _ as usize);
     let rip = unsafe { *ptr_1 }();
     println!("from_asm: {rip:#x}");
+    println!("asm_code: {:x?}", unsafe { *ptr.cast::<&[u8; 9]>() });
 }
