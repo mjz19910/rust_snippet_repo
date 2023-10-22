17140027c7f842d3de0d8b1c4b9650b88035b918
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-21 17:26:28 -0700

    u

--- src/modes/do_asm_get_rip.rs
+++ src/modes/do_asm_get_rip.rs
@@ -4,7 +4,7 @@ use std::arch::asm;
 pub fn asm_get_rip() -> usize {
     let mut value: usize;
     unsafe {
-        asm!("call 1f", "1:", "pop {0}", "lea {0},[{0}-5]", out(reg) value, options(nostack));
+        asm!("call 1f", "1:", "pop {0}", out(reg) value, options(nostack));
     }
     value
 }
@@ -15,7 +15,15 @@ pub fn do_asm_get_rip() {
     println!("fn_ptr  : {:#x}", asm_get_rip as fn() -> _ as usize);
     let fn_ = unsafe { *ptr };
     let rip = fn_();
-    assert_eq!(fn_ as usize, rip);
     println!("from_asm: {rip:#x}");
-    println!("asm_code: {:x?}", unsafe { *ptr.cast::<&[u8; 5+1+4+1]>() });
+    let v_ptr = unsafe { *ptr.cast::<&u8>() };
+    if v_ptr == &0xe8 {
+        println!("asm_code: {:x?}", unsafe {
+            *ptr.cast::<&[u8; 5 + 1 + 1]>()
+        });
+    } else {
+        println!("asm_code: {:x?}", unsafe {
+            *ptr.cast::<&[u8; 1 + 5 + 1 + 4 + 4 + 1 + 1]>()
+        });
+    }
 }
