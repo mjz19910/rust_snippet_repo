68de1f3e2a543a7d8e3dbc33d8dfc47518b3dfcb
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-24 21:01:34 -0700

    u

--- examples/traits.rs
+++ examples/traits.rs
@@ -1,11 +1,8 @@
-use std::{
-    fmt::{Debug, Display},
-    mem::ManuallyDrop,
-};
+use std::fmt::{Debug, Display};
 
 trait NewTrait: Debug + Display {
     type Output;
-    fn get(&self) -> ManuallyDrop<Self::Output>;
+    fn get(&self) -> Self::Output;
 }
 
 impl<T> NewTrait for T
@@ -13,13 +10,13 @@ where
     T: Debug + Display,
 {
     type Output = T;
-    fn get(&self) -> ManuallyDrop<T> {
+    fn get(&self) -> T {
         let v = self as *const T;
-        ManuallyDrop::new(unsafe { std::ptr::read(v) })
+        unsafe { std::ptr::read(v) }
     }
 }
 fn main() {
     let a: (i32, Box<dyn NewTrait<Output = _>>) = (1, Box::new(2));
-    let v = a.1.get();
-    println!("{}, {:?}", a.0, v.as_ref());
+    let v = (*a.1).get();
+    println!("{}, {:?}", a.0, v);
 }
