517208b93873150929ea1eeb6dd914471c895ea3
Matthias Zimmerman <matthias291999@gmail.com>
2023-10-24 20:42:41 -0700

    u

--- examples/traits.rs
+++ examples/traits.rs
@@ -1,10 +1,18 @@
 use std::fmt::{Debug, Display};
 
-trait NewTrait: Debug + Display {}
+trait NewTrait: Debug + Display {
+    type Output;
+    fn get(&self) -> Self::Output;
+}
 
-impl NewTrait for i32 {}
-
+impl NewTrait for i32 {
+    type Output = i32;
+    fn get(&self) -> i32 {
+        *self
+    }
+}
 fn main() {
-    let a: (i32, Box<dyn NewTrait>) = (1, Box::new(2));
-    println!("{}, {}", a.0, a.1);
+    let a: (i32, Box<dyn NewTrait<Output = _>>) = (1, Box::new(2));
+    let v: i32 = a.1.get();
+    println!("{}, {}", a.0, v);
 }
