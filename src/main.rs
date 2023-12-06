34300dbe26c884a850978f8fef74c03e5f6d98a0
Matthias Zimmerman <matthias291999@gmail.com>
2023-12-06 04:38:47 -0700

    u

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,35 @@ pub mod modes;
 pub mod support;
 
 use support::{
-    async_vec,
-    constants::DEBUG_ENABLED,
-    get_command_line_arguments, ArgParser, PtrIter,
+    async_vec, constants::DEBUG_ENABLED, get_command_line_arguments, ArgParser, PtrIter,
 };
 
 use crate::modes::{do_asm_get_rip, lambda_moved, lambda_ref, print_dl_addr_info, read_main_ptr};
+
+fn run_one(function_name: &str) -> Result<(), String> {
+    match function_name {
+        "async_vec" => async_vec(),
+
+        // For modes modules
+        "do_asm_get_rip" => do_asm_get_rip(),
+        "lambda_ref" => lambda_ref(),
+        "lambda_moved" => lambda_moved(),
+        "print_dl_addr_info" => print_dl_addr_info(),
+        "ptr_iter_run" => PtrIter::new()?.run()?,
+        "read_main_ptr" => read_main_ptr(),
+        "array_1" => {
+            let x = [40, 100];
+            println!("{x:?}");
+        }
+
+        // None
+        "none" => (),
+
+        // Error
+        _ => return Err(format!("Unknown function `{}`", function_name)),
+    }
+    Ok(())
+}
 
 pub fn main() -> Result<(), String> {
     let args = get_command_line_arguments()?;
@@ -25,23 +48,7 @@ pub fn main() -> Result<(), String> {
         DEBUG_ENABLED = parsed_args.debug_enabled;
     }
     for func_name in parsed_args.run_options {
-        match func_name.as_str() {
-            "async_vec" => async_vec(),
-
-            // For modes modules
-            "do_asm_get_rip" => do_asm_get_rip(),
-            "lambda_ref" => lambda_ref(),
-            "lambda_moved" => lambda_moved(),
-            "print_dl_addr_info" => print_dl_addr_info(),
-            "ptr_iter_run" => PtrIter::new()?.run()?,
-            "read_main_ptr" => read_main_ptr(),
-
-            // None
-            "none" => (),
-
-            // Error
-            _ => return Err(format!("Unknown function `{}`", func_name)),
-        }
+        run_one(func_name.as_str())?;
     }
     if parsed_args.gdb_delay_loop {
         loop {}
